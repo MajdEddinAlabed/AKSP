9f23e3e861cffeeef086e2f04e4d75878b364a28
Majd Eddin Alabed <majdeddin6@gmail.com>
2023-12-03 17:46:15 +0200

    added ask button to components

--- src/app/q/page.tsx
+++ src/app/q/page.tsx
@@ -5,6 +5,7 @@ import { JSX } from "react";
 import "/src/lib/general.css";
 import { QuestionReadDto } from "@/src/lib/types";
 import Link from "next/link";
+import askButton from "@/src/components/buttons/askButton";
 
 function QuestionPreview(question: QuestionReadDto) {
   return (
@@ -32,9 +33,7 @@ export default async function Questions() {
           <h1 className="text-3xl font-serif">Top Questions</h1>
         </div>
         <div className="mx-60 my-20"></div>
-        <div className="flex items-center justify-center w-52 max-w-xs">
-          <button className="button">Ask Question</button>
-        </div>
+        {askButton()}
       </div>
       <div className="my-5"></div>
       <div className="flex flex-col items-center justify-center h-1/2">
---
+++ src/components/buttons/askButton.tsx
@@ -0,0 +1,7 @@
+export default function askButton() {
+  return (
+    <div className="flex items-center justify-center w-52 max-w-xs">
+      <button className="button">Ask Question</button>
+    </div>
+  );
+}
